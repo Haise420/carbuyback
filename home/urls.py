b883d660f8367604b15a399060b2b31ea8a011fb
Haise420 <nickesoldier@gmail.com>
2024-10-16 00:42:32 +0200

    1.3v Client Website

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-
- 
+    path('usluge', views.usluge, name="usluge"),
+    path('novosti', views.novosti, name="novosti"),
+    path('kontakt', views.kontakt, name="kontakt"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
+
+def usluge(request):
+    return render(request, 'home/usluge.html')
+
+def novosti(request):
+    return render(request, 'home/novosti.html')
+
+def kontakt(request):
+    return render(request, 'home/kontakt.html')
